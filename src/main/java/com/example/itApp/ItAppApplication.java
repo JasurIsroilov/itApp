76e013df8c69a786c9dabebab4fbd6df6c1da0a3
JasurIsroilov <woot_1999@mail.ru>
2024-06-25 17:01:50 +0300

    Project base commit

---
+++ src/main/java/com/example/itApp/ItAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.itApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ItAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/itApp/ItAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.itApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
